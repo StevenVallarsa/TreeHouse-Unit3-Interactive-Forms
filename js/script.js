4a5dd96a32be546eca88f7f43f91e95683a09f42
StevenVallarsa <orionsjv@gmail.com>
2020-01-30 13:21:18 -0500

    Added pop up menu functionality

--- js/script.js
+++ js/script.js
@@ -1,22 +1,48 @@
 /* * *
- *  Variables to attach event listeners to
+ *  Allow only letters for first character of words, then letters,
+ *  periods and hyphens for other characters. Case insensitive.
  * * */
-
 const nameField = document.getElementById("name");
-// Validation first letter caps for each word.
-// Allow periods and hyphens for other characters.
 const nameValidation = /^[a-z][-.a-z]*( [a-z][-.a-z]*)*$/i;
 
+/* * *
+ * USERNAME:
+ *  Allow only letters for first character, then letters, numbers,
+ *  dots, hyphens and underscores. Case insensitive.
+ * AT SYMBOL (@)
+ * DOMAIN:
+ *  Letter or number for first character, then letters, numbers, dots, and hyphens.
+ * DOT (.)
+ * TOP-LEVEL DOMAIN:
+ *  Only letters.
+ * * */
 const emailField = document.getElementById("mail");
-// Validation for first character of username being a letter
-// with other characters being letters, numbers, dots, hyphens and underscores.
-//
-const emailValidation = /^[a-z][-.\w]*@[a-z0-9][-.\w]+\.[a-z]+$/i;
+const emailValidation = /^[a-z][-.\w]*@[a-z0-9][-.\a-z0-9]+\.[a-z]+$/i;
 
+/* * *
+ *
+ * * */
 const titleMenu = document.getElementById("title");
+const otherTitle = document.getElementById("other-title");
+otherTitle.style.display = "none";
+const optionTitle = document.createElement("option");
+optionTitle.textContent = "Select Your Job Role";
+optionTitle.setAttribute("disabled", true);
+optionTitle.setAttribute("selected", true);
+optionTitle.setAttribute("hidden", false);
+titleMenu.setAttribute("onchange", "otherSelection()");
+const popUpTitle = titleMenu.prepend(optionTitle);
+
+function otherSelection() {
+  if (titleMenu.value === "other") {
+    otherTitle.style.display = "";
+  } else {
+    otherTitle.style.display = "none";
+  }
+}
+
 const form = document.querySelector("form");
 
-// create variables to set the event listners on
 // Regex for fields
 // job role popup menu options
 // checkbox connection to activities

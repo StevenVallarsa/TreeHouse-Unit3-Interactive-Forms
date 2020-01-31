a4c8671531e32496fde1b3a2e61ebb030d95f518
StevenVallarsa <orionsjv@gmail.com>
2020-01-30 21:51:36 -0500

    Tried to get t-shirt selection to work

--- js/script.js
+++ js/script.js
@@ -1,36 +1,43 @@
 /* * *
+ * NAME FIELD VALIDATION
  *  Allow only letters for first character of words, then letters,
  *  periods and hyphens for other characters. Case insensitive.
  * * */
+
 const nameField = document.getElementById("name");
 const nameValidation = /^[a-z][-.a-z]*( [a-z][-.a-z]*)*$/i;
 
 /* * *
- * USERNAME:
+ * EMAIL FIELD VALIDATION
+ * Username:
  *  Allow only letters for first character, then letters, numbers,
  *  dots, hyphens and underscores. Case insensitive.
  * AT SYMBOL (@)
  * DOMAIN:
  *  Letter or number for first character, then letters, numbers, dots, and hyphens.
  * DOT (.)
- * TOP-LEVEL DOMAIN:
- *  Only letters.
+ * TOP-LEVEL DOMAIN: Only letters.
  * * */
+
 const emailField = document.getElementById("mail");
 const emailValidation = /^[a-z][-.\w]*@[a-z0-9][-.\a-z0-9]+\.[a-z]+$/i;
 
 /* * *
- *
+ * Pop Up Job Menu -- Will show input box when "Other" selected from menu
  * * */
+
 const titleMenu = document.getElementById("title");
+titleMenu.setAttribute("onchange", "otherSelection()");
+
 const otherTitle = document.getElementById("other-title");
 otherTitle.style.display = "none";
+
 const optionTitle = document.createElement("option");
 optionTitle.textContent = "Select Your Job Role";
 optionTitle.setAttribute("disabled", true);
 optionTitle.setAttribute("selected", true);
 optionTitle.setAttribute("hidden", false);
-titleMenu.setAttribute("onchange", "otherSelection()");
+
 const popUpTitle = titleMenu.prepend(optionTitle);
 
 function otherSelection() {
@@ -41,17 +48,45 @@ function otherSelection() {
   }
 }
 
-const form = document.querySelector("form");
+const tshirtDesign = document.getElementById("design");
+tshirtDesign.setAttribute("onchange", "selectStyle()");
 
-// Regex for fields
-// job role popup menu options
-// checkbox connection to activities
-// payment options
+const tshirtFirstChild = tshirtDesign.firstElementChild;
+tshirtFirstChild.setAttribute("disabled", true);
+tshirtFirstChild.setAttribute("selected", true);
+tshirtFirstChild.setAttribute("hidden", false);
+
+const tshirtOptions = document.querySelectorAll("#color option");
+
+function selectStyle() {
+  for (let i = 0; i < tshirtOptions.length; i++) {
+    if (
+      tshirtOptions[i].textContent.includes("JS shirt") &&
+      event.target.value === "heart js"
+    ) {
+      tshirtOptions[i].disabled = true;
+      console.log("hidden");
+    } else {
+      console.log("NO");
+    }
+  }
+}
+
+/* * *
+ * Event Listner for form validation
+ * * */
+
+const form = document.querySelector("form");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
   testFields(nameValidation, nameField);
   testFields(emailValidation, emailField);
+  if (titleMenu.value === "Select Your Job Role") {
+    titleMenu.style.borderColor = "red";
+  } else {
+    titleMenu.style.borderColor = "";
+  }
 });
 
 function testFields(regex, element) {
